6c23e1555eff64de4aaedf37b7fbde65c346a89d
Suhaib Khater <s.khater@classera.com>
2024-04-12 10:35:44 -0500

    init commit

---
+++ src/test/java/com/ads/ADS/AdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ads.ADS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
